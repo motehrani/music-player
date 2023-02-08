9197fc384bf20716bd31e95fe320a1ff938e9dfa
htehrani98 <h.tehrani1377@gmail.com>
2023-02-08 20:28:06 +0330

    create all components and define every function and whole structure via comment

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -7,6 +7,37 @@ import { Player } from '@src/shared/components/player'
 import { Library } from '@src/shared/components/library'
 
 const Home = () => {
+  // Ref
+  const audioRef = useRef(null)
+  // state
+  const [songs, setSongs] = useState(data())
+  const [currentSong, setCurrentSong] = useState(songs[0])
+  const [isPlaying, setIsPlaying] = useState(false)
+  const [libraryStatus, setLibraryStatus] = useState(false)
+  const [songInfo, setSongInfo] = useState({
+    currentTime: '',
+    duration: '',
+    animationPercentage: 0,
+  })
+  const timeUpdateHandler = e => {
+    const current = e.target.currentTime
+    const duration = e.target.duration
+    //Calculate Percentage
+    const roundedCurrent = Math.round(current)
+    const roundedDuration = Math.round(duration)
+    const animation = Math.round((roundedCurrent / roundedDuration) * 100)
+    setSongInfo({
+      ...songInfo,
+      currentTime: current,
+      duration,
+      animationPercentage: animation,
+    })
+  }
+  const songEndHandler = async () => {
+    let currentIndex = songs.findIndex(song => song.id === currentSong.id)
+    await setCurrentSong(songs[(currentIndex + 1) % songs.length])
+    if (isPlaying) audioRef.current.play()
+  }
   return (
     <div>
       {/* <Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus} /> */}
